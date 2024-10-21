0d446587a393828cf56ca3c20adf9b44ec84ea52
Luispmv <luispmv07@hotmail.com>
2024-10-21 03:21:35 -0600

    Editando la primera vista con una funcion llamada index()

--- notes_MujicaTavera/views.py
+++ notes_MujicaTavera/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse
+from .models import Note
+from django.template import loader
+from django.db.models import F
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+    notas = Note.objects.all()
+    context = {
+        "notas": notas
+    }
+    return render(request, "notes_MujicaTavera/index.html", context)
