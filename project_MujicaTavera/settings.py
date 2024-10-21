efb0a346c266612c263db2ff68935990f94b7d7c
Luispmv <luispmv07@hotmail.com>
2024-10-21 02:04:59 -0600

    Modificando settings.py en la seccion de INSTALLED APPS me falto una coma jajaja

--- project_MujicaTavera/settings.py
+++ project_MujicaTavera/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'notes_MujicaTavera'
+    'notes_MujicaTavera',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
