6aa9a6b2cd09fbd0ca6a5ae844e5847e30cddab3
Luispmv <luispmv07@hotmail.com>
2024-10-21 04:10:00 -0600

    Agregando url de la vista individual

--- notes_MujicaTavera/urls.py
+++ notes_MujicaTavera/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "notes_MujicaTavera"
 
 urlpatterns = [
-    #notas --> mostrar todas las notas existentes
-    path("", views.index, name="index")
+    # 127.0.0.1:8000/notes_MujicaTavera/
+    path("", views.index, name="index"),
+
+    # mostrar nota individual
+    path("<int:nota_id>/", views.individual, name="individual")
 ]
