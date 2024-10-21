2407e69d4e04ec1f61e259e8c9c76b3046d7a4b4
Luispmv <luispmv07@hotmail.com>
2024-10-21 01:32:37 -0600

    Agregando el modelo Note en models.py

--- notes_MujicaTavera/models.py
+++ notes_MujicaTavera/models.py
@@ -1,3 +1,16 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# Creacion de un modelo llamdo Note
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
