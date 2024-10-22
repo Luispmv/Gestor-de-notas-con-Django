092975ac778e5569ccc37032ebf29c6a4e72f3c7
KevTav20 <kevin_tavera11@yahoo.com>
2024-10-22 15:20:29 -0600

    Diseño a la app

--- notes_MujicaTavera/views.py
+++ notes_MujicaTavera/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .models import Note
 from django.contrib.auth.models import User
 from random import choice
